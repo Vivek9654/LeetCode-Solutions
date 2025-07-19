c756d2622857ff5998631c3838568198ef443963
Vivek9654 <166800866+Vivek9654@users.noreply.github.com>
2025-07-19 15:37:31 +0530

    43 ms (62.11%)

---
+++ LeetCode/Medium/1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        
+        List<String> result = new ArrayList<>();
+        result.add(folder[0]);
+
+        for (int i = 1; i < folder.length; i++) {
+            String currentFolder = folder[i];
+            String lastParent = result.get(result.size() - 1);
+
+            if (!currentFolder.startsWith(lastParent + "/")) {
+                result.add(currentFolder);
+            }
+        }
+        
+        return result;
+    }
+}
