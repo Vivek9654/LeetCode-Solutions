5d62c5bf239c0046f8862cb8bfb561555101bbe8
VIVEK SINGH <166800866+Vivek9654@users.noreply.github.com>
2025-08-06 00:48:57 +0530

    31 ms (5.43%)

---
+++ LeetCode/Easy/3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
@@ -0,0 +1,45 @@
+// class Solution {
+//     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+//         int n = fruits.length,used=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(fruits[i]<=baskets[j])
+//                 {
+//                     used++;
+//                     baskets[j] = -1;
+//                     break;
+//                 }
+//             }
+//         }
+//         return n - used;
+//     }
+// }
+
+
+
+
+// above one was very bruite force approach now lets optimise it further
+
+// now below solution is optimise for O(nlogn)
+
+
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] bask) {
+        int n = fruits.length;
+        TreeSet<Integer> aval = new TreeSet<>();
+        for (int i = 0; i < n; i++) aval.add(i);
+        int used = 0;
+        for (int f : fruits) {
+            Integer idx = aval.ceiling(0);
+            while (idx != null && bask[idx] < f) idx = aval.higher(idx);
+            if (idx != null) {
+                used++;
+                aval.remove(idx);
+            }
+        }
+        return n - used;
+    }
+}
+
