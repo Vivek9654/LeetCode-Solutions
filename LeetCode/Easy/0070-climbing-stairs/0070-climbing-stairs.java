b207aa9c4ec367b38ac38d9fe1a8dd77c9c500d7
VIVEK SINGH <166800866+Vivek9654@users.noreply.github.com>
2025-08-10 00:39:39 +0530

    0 ms (100%)

---
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int climbStairs(int n) {
+      //iterative approach
+        //  int step1= 1, step2=2;
+
+
+        //  for(int i=3;i<=n;i++){
+        //    int step3= step1+step2;
+        //    step1= step2;
+        //    step2= step3;
+
+        //  }
+        
+        // return step2;
+
+
+        // now i am doing it with dp
+
+        int[] dp = new int[n+1];
+
+        if(n==1) return 1;
+       dp[1]=1;
+       dp[2]=2;
+
+       for(int i=3;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+       }
+         return dp[n];
+    }
+}
