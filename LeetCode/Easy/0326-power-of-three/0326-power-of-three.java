0006dc2e5ce2e051f9b2293eaf66b5ebd24ed7e0
VIVEK SINGH <166800866+Vivek9654@users.noreply.github.com>
2025-08-18 09:38:35 +0530

    8 ms (82.04%)

--- LeetCode/Easy/0326-power-of-three/0326-power-of-three.java
+++ LeetCode/Easy/0326-power-of-three/0326-power-of-three.java
@@ -1,7 +1,22 @@
+// class Solution {
+//     public boolean isPowerOfThree(int n) {
+//         if(n<0) return false;
+//         for(int i=0;i<n;i++){
+//         int pow=(int)Math.pow(3,i);
+//       if(pow==n) return true;
+//         }
+        
+//         return false;
+//     }
+// }
+
+
 class Solution {
     public boolean isPowerOfThree(int n) {
         if (n <= 0) return false;
-        while (n % 3 == 0) n /= 3;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
         return n == 1;
     }
 }
