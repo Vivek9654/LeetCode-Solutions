620484e0a68aa92400f8fa976092ac78aff1eb45
VIVEK SINGH <166800866+Vivek9654@users.noreply.github.com>
2025-09-02 00:01:46 +0530

    343 ms (64.25%)

---
+++ LeetCode/Medium/1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.java
@@ -0,0 +1,38 @@
+class Solution {
+    public double maxAverageRatio(int[][] classes, int extraStudents) {
+        int n = classes.length;
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+
+        for (int i = 0; i < n; i++) {
+            int pass = classes[i][0];
+            int total = classes[i][1];
+            double currPR = (double) pass / total;
+            double nextPR = (double) (pass + 1) / (total + 1);
+            double gainPR = nextPR - currPR;
+            pq.add(new double[]{gainPR, i});
+        }
+
+        while (extraStudents > 0) {
+            double[] curr = pq.poll();
+            int idx = (int) curr[1];
+
+            classes[idx][0]++;
+            classes[idx][1]++;
+
+            int pass = classes[idx][0];
+            int total = classes[idx][1];
+            double currPR = (double) pass / total;
+            double nextPR = (double) (pass + 1) / (total + 1);
+            double gainPR = nextPR - currPR;
+
+            pq.add(new double[]{gainPR, idx});
+            extraStudents--;
+        }
+
+        double res = 0d;
+        for (int i = 0; i < n; i++) {
+            res += (double) classes[i][0] / classes[i][1];
+        }
+        return res / n;
+    }
+}
