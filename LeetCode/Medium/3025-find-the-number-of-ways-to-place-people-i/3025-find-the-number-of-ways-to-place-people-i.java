65ae49bb57cf0b634f4cc4a20a86958930880cf4
VIVEK SINGH <166800866+Vivek9654@users.noreply.github.com>
2025-09-02 23:35:11 +0530

    6 ms (73.13%)

---
+++ LeetCode/Medium/3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.java
@@ -0,0 +1,34 @@
+ 
+// i have to Sort the points by x (ascending), and by y (descending) if x is the same.
+//  nUse a double loop to count valid pairs, making sure no other point lies between them.
+
+import java.util.*;
+
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        // Sort by x ascending, y descending
+        Arrays.sort(points, (a, b) -> {
+            if (a[0] == b[0]) return Integer.compare(b[1], a[1]);
+            return Integer.compare(a[0], b[0]);
+        });
+
+        int n = points.length;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int top = points[i][1];
+            int bot = Integer.MIN_VALUE;
+
+            for (int j = i + 1; j < n; j++) {
+                int y = points[j][1];
+                if (bot < y && y <= top) {
+                    result++;
+                    bot = y;
+                    if (bot == top) break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
