ceff668c96ba43f3f9a750f368f2b68a94825800
VIVEK SINGH <166800866+Vivek9654@users.noreply.github.com>
2025-09-08 01:09:53 +0530

    0 ms (100%)

---
+++ LeetCode/Easy/1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] sumZero(int n) {
+
+ 
+    int[] result = new int[n];
+        int index = 0;
+
+       //check if number is ever or not
+        if (n % 2 != 0) {
+            result[index++] = 0;
+        }
+
+       //ss
+        for (int i = 1; index < n; i++) {
+            result[index++] = i;  
+            if (index < n) {
+                result[index++] = -i;  
+            }
+        }
+
+        return result;
+       
+
+
+     
+    }
+
+}
