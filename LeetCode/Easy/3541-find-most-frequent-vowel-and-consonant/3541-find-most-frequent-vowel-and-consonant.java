8ca6b984f1c4f3222ab06f41461c436abe7855e3
VIVEK SINGH <166800866+Vivek9654@users.noreply.github.com>
2025-09-13 15:53:05 +0530

    1 ms (100%)

--- LeetCode/Easy/3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
+++ LeetCode/Easy/3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
@@ -7,26 +7,36 @@ class Solution {
     public int maxFreqSum(String s) {
 
         int[] freq = new int[26];
-         int maxvow=0,maxconst=0;
+        int maxvow = 0, maxconst = 0;
         for (char ch : s.toCharArray()) {
             freq[ch - 'a']++;
         }
 
+        // for(int i=0;i<26;i++){
+        //     char ch=(char) (i+'a');
+        //     if(isVowel(ch)){
+        //         if(freq[i]>maxvow){
+        //             maxvow= freq[i];
+        //         }
+        //     }else{
+        //         if(freq[i]>maxconst){
+        //             maxconst=freq[i];
 
-        for(int i=0;i<26;i++){
-            char ch=(char) (i+'a');
-            if(isVowel(ch)){
-                if(freq[i]>maxvow){
-                    maxvow= freq[i];
-                }
-            }else{
-                if(freq[i]>maxconst){
-                    maxconst=freq[i];
+        //         }
+        //     }
+        // }
 
-                }
-            }
+        ///little but more optimised
+        for (int i = 0; i < 26; i++) {
+            char ch = (char) (i + 'a');
+
+            if (isVowel(ch))
+                maxvow = Math.max(maxvow, freq[i]);
+
+            else
+                maxconst = Math.max(maxconst, freq[i]);
         }
 
-       return maxvow+maxconst;
+        return maxvow + maxconst;
     }
 }
